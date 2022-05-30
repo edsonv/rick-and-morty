bb5141b51c6e0fc2905ecb8a60f46ba9fb92a008
Edson J Vargas <edson.jhair@yahoo.com>
2022-05-29 20:17:43 -0500

    Updates

---
+++ precache-manifest.54405344e6b287eb59f348a6211c9252.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "781b51fe8896ec137a35",
+    "url": "/static/css/main.98323d09.chunk.css"
+  },
+  {
+    "revision": "781b51fe8896ec137a35",
+    "url": "/static/js/main.781b51fe.chunk.js"
+  },
+  {
+    "revision": "d6d9feaf115dfbd6036c",
+    "url": "/static/js/1.d6d9feaf.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "de82c64b0013c1e2a22f5a8fb26f04f4",
+    "url": "/static/media/logo.de82c64b.png"
+  },
+  {
+    "revision": "f7212607e933e889ed18216d0d3658df",
+    "url": "/index.html"
+  }
+];
